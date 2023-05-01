38331d25546147bad5fd8f2fe0736272481068c8
unknown <hooda.darknesss@gmail.com>
2023-05-01 03:07:19 +0200

    f

--- buttons.ino
+++ buttons.ino
@@ -1,7 +1,7 @@
 
 #define btn_num 3
 //12 d6
-int btn [btn_num] = {5, 4, 14}; //d1 d2 d5 
+int btn [btn_num] = {5, 4, 14}; //d1 d2 d5
 bool btn_old_sg [btn_num] = {1, 1, 1};
 
 
@@ -19,5 +19,5 @@ bool getBtn(int i) {
   bool sg = digitalRead(btn[i]);
   bool r = btn_old_sg[i] != sg && sg == HIGH;
   btn_old_sg[i] = sg;
-  return r;
+  return !sg;
 }
--- logic.ino
+++ logic.ino
@@ -1,5 +1,13 @@
 unsigned long  timer = 0;
+unsigned long  timer2 = 0;
 #define timer_time_out  5000
+#define timer_time_out2  30000
+
+
+bool flag1 = false;
+bool flag2 = false;
+bool flag3 = false;
+
 
 #define emcbtn   0
 #define forkbtn  1
@@ -10,20 +18,67 @@ void handler() {
 
   if (getBtn(emcbtn)) {
     // digitalWrite(relay, HIGH);
-    Serial.println(emcbtn);
-    if (getBtn(forkbtn)) {
-      timer = millis();
-      Serial.println(emcbtn);
+    if (!flag1)
+    {
+      Serial.println("first !");
+      flag1 = true;
     }
+
+    if (getBtn(forkbtn)) {
+      if (!flag2)
+      {
+        Serial.println("sec !");
+        flag2 = true;
+        timer = millis();
+        Serial.println("start timer !");
+        timer2 = millis();
+      }
+
+
+    }
+    else {
+      timer = 0;
+      timer2 = 0;
+      Serial.println("reset timer !");
+      flag2 = false;
+      flag3 = false;
+    }
+
     if (timer &&  millis() < timer + timer_time_out) {
       if (getBtn(lockbtn)) {
-        Serial.println(lockbtn);
-        digitalWrite(relay, HIGH);
+        if (!flag3)
+        {
+          Serial.println("third");
+          flag3 = true;
+          Serial.println("not any more !!! ");
+          digitalWrite(relay, HIGH);
+        }
+
+
       }
     }
+    else if (timer &&  millis() > timer + timer_time_out) {
+      if (getBtn(lockbtn)) {
+        if (!flag3)
+        {
+          Serial.println("third");
+          flag3 = true;
+          Serial.println("al7a msa !!! ");
+          digitalWrite(relay, LOW);
+        }
+
+
+      }
+    }
+
+    if (timer2 &&  millis() > timer2 + timer_time_out2) {
+      digitalWrite(relay, HIGH);
+    }
+
   }
   else {
-    timer = 0;
+    digitalWrite(relay, HIGH);
+    flag1 = false;
   }
 
 }
